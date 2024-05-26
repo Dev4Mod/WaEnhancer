584e9ef14f5ae50286cc04dc0196397a06733ab1
Dev4Mod <wellingtonmods@gmail.com>
2024-05-26 10:41:27 -0300

    * Add Anti Updater

---
+++ app/src/main/java/com/wmods/wppenhacer/xposed/AntiUpdater.java
@@ -0,0 +1,31 @@
+package com.wmods.wppenhacer.xposed;
+
+import android.content.pm.PackageInstaller;
+
+import com.wmods.wppenhacer.xposed.core.MainFeatures;
+
+import java.io.IOException;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+
+public class AntiUpdater {
+
+    public static void hookSession(XSharedPreferences prefs) {
+        XposedBridge.hookAllMethods(PackageInstaller.class, "createSession", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                var session = (PackageInstaller.SessionParams) param.args[0];
+                var packageName = XposedHelpers.getObjectField(session, "mPackageName");
+                if (packageName.equals(MainFeatures.PACKAGE_WPP) || packageName.equals(MainFeatures.PACKAGE_BUSINESS)) {
+                    if (prefs.getBoolean("lockupdate", false)) {
+                        param.setThrowable(new IOException("UPDATE LOCKED BY WAENHANCER"));
+                    }
+                }
+            }
+        });
+
+    }
+}
