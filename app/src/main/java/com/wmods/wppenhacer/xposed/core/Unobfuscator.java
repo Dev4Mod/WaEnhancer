cec010822238b438a6ac5b10f3108b9f0d436b75
Dev4Mod <wellingtonmods@gmail.com>
2024-05-27 13:13:10 -0300

    Add function to disable default Emojis

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/Unobfuscator.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/Unobfuscator.java
@@ -1505,4 +1505,12 @@ public class Unobfuscator {
         }
         throw new RuntimeException("FieldExpireTime method not found");
     }
+
+    public static Method loadDefEmojiClass(ClassLoader loader) throws Exception {
+        return UnobfuscatorCache.getInstance().getMethod(loader, () -> {
+            var method = findFirstMethodUsingStrings(loader, StringMatchType.Contains, "emojis.oba");
+            if (method == null) throw new RuntimeException("DefEmoji class not found");
+            return method;
+        });
+    }
 }
