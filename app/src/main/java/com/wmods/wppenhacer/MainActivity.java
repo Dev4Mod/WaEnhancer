0d5b5fe7bd7f62ec1e8c4625f46d6ec62bc77cf6
Dev4Mod <wellingtonmods@gmail.com>
2024-05-29 16:27:23 -0300

    Add support to swipe tabs in Wa Enhancer

--- app/src/main/java/com/wmods/wppenhacer/MainActivity.java
+++ app/src/main/java/com/wmods/wppenhacer/MainActivity.java
@@ -1,17 +1,24 @@
 package com.wmods.wppenhacer;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.wmods.wppenhacer.adapter.MainPagerAdapter;
 import com.wmods.wppenhacer.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
@@ -33,13 +40,54 @@ public class MainActivity extends AppCompatActivity {
         }
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main);
-        if (navHostFragment == null) return;
-        NavController navController = navHostFragment.getNavController();
-        var nav = binding.navView;
-        NavigationUI.setupWithNavController(nav, navController);
-        ArrayList<String> permissions = new ArrayList<>();
 
+        MainPagerAdapter pagerAdapter = new MainPagerAdapter(this);
+        binding.viewPager.setAdapter(pagerAdapter);
+
+        binding.navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return switch (item.getItemId()) {
+                    case R.id.navigation_home -> {
+                        binding.viewPager.setCurrentItem(0);
+                        yield true;
+                    }
+                    case R.id.navigation_chat -> {
+                        binding.viewPager.setCurrentItem(1);
+                        yield true;
+                    }
+                    case R.id.navigation_privacy -> {
+                        binding.viewPager.setCurrentItem(2);
+                        yield true;
+                    }
+                    case R.id.navigation_media -> {
+                        binding.viewPager.setCurrentItem(3);
+                        yield true;
+                    }
+                    case R.id.navigation_colors -> {
+                        binding.viewPager.setCurrentItem(4);
+                        yield true;
+                    }
+                    default -> false;
+                };
+            }
+        });
+
+        binding.viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                binding.navView.getMenu().getItem(position).setChecked(true);
+            }
+        });
+
+        setupPermissions();
+        FilePicker.registerFilePicker(this);
+    }
+
+    private void setupPermissions() {
+        ArrayList<String> permissions = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             permissions.add(Manifest.permission.READ_MEDIA_IMAGES);
         }
@@ -49,7 +97,6 @@ public class MainActivity extends AppCompatActivity {
         if (!permissions.isEmpty()) {
             ActivityCompat.requestPermissions(this, permissions.toArray(new String[0]), 0);
         }
-        FilePicker.registerFilePicker(this);
     }
 
     public static boolean isXposedEnabled() {
