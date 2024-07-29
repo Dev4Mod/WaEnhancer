50b40bb33c790a1dedb2c03d21a423c868fc48ca
Dev4Mod <wellingtonmods@gmail.com>
2024-07-29 17:20:56 -0300

    improve bypass in Scope Visibility

---
+++ app/src/main/java/com/wmods/wppenhacer/receivers/WAFReceiver.java
@@ -0,0 +1,12 @@
+package com.wmods.wppenhacer.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WAFReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // nothing here, just a placeholder
+    }
+}
