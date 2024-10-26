4abd5117ff68446724869498d35ec7fd50b52f7a
Dev4Mod <wellingtonmods@gmail.com>
2024-10-26 14:44:01 -0300

    Fix text stories in IGStatus

--- app/src/main/java/com/wmods/wppenhacer/adapter/IGStatusAdapter.java
+++ app/src/main/java/com/wmods/wppenhacer/adapter/IGStatusAdapter.java
@@ -97,7 +97,11 @@ public class IGStatusAdapter extends ArrayAdapter {
 
                 tabdialog.addTab(activity.getString(ResId.string.edit_text), iconEdit, (view) -> {
                     Intent A09 = new Intent();
-                    A09.setClassName(activity.getPackageName(), "com.whatsapp.textstatuscomposer.TextStatusComposerActivityV2");
+                    if (XposedHelpers.findClassIfExists("com.whatsapp.textstatuscomposer.TextStatusComposerActivity", activity.getClassLoader()) != null) {
+                        A09.setClassName(activity.getPackageName(), "com.whatsapp.textstatuscomposer.TextStatusComposerActivity");
+                    } else {
+                        A09.setClassName(activity.getPackageName(), "com.whatsapp.textstatuscomposer.TextStatusComposerActivityV2");
+                    }
                     activity.startActivity(A09);
                     dialog.dismissDialog();
                 });
