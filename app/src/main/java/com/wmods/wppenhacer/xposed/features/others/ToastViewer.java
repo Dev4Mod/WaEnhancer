acf20967800d7b0ec7f694c45c84731e5c005e4b
Dev4Mod <wellingtonmods@gmail.com>
2025-02-26 22:38:45 -0300

    Remove logs
    Signed-off-by: Dev4Mod <wellingtonmods@gmail.com>

--- app/src/main/java/com/wmods/wppenhacer/xposed/features/others/ToastViewer.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/others/ToastViewer.java
@@ -12,7 +12,6 @@ import com.wmods.wppenhacer.xposed.core.components.FMessageWpp;
 import com.wmods.wppenhacer.xposed.core.db.MessageStore;
 import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator;
 import com.wmods.wppenhacer.xposed.features.general.Tasker;
-import com.wmods.wppenhacer.xposed.utils.DebugUtils;
 import com.wmods.wppenhacer.xposed.utils.ReflectionUtils;
 import com.wmods.wppenhacer.xposed.utils.ResId;
 import com.wmods.wppenhacer.xposed.utils.Utils;
@@ -51,7 +50,6 @@ public class ToastViewer extends Feature {
         XposedBridge.hookMethod(onInsertReceipt, new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                DebugUtils.debugArgs(param.args);
                 int type = ReflectionUtils.getArg(param.args, Integer.class, 0);
                 long id = ReflectionUtils.getArg(param.args, Long.class, 0);
                 if (type != 13) return;
@@ -90,12 +88,9 @@ public class ToastViewer extends Feature {
 
     private synchronized void checkDataBase(SQLiteDatabase sql, long id, String contactName, String raw, boolean toastViewedMessage, boolean toast_viewed_status) {
         try (var result2 = sql.query("message", null, "_id = ?", new String[]{String.valueOf(id)}, null, null, null)) {
-            log(result2);
             if (!result2.moveToNext()) return;
-            log("OK1");
 
             var participantHash = result2.getString(result2.getColumnIndexOrThrow("participant_hash"));
-            log(participantHash);
             if (participantHash != null) {
                 if (toast_viewed_status) {
                     Utils.showToast(Utils.getApplication().getString(ResId.string.viewed_your_status, contactName), Toast.LENGTH_LONG);
