98f63d844aaf774fb60a8cbdd3540534d650f349
Dev4Mod <wellingtonmods@gmail.com>
2026-01-11 13:55:24 -0300

    Refactor activity lifecycle management to prevent memory leaks

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/WaCallback.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/WaCallback.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+
 public class WaCallback implements Application.ActivityLifecycleCallbacks {
+
     private static void triggerActivityState(@NonNull Activity activity, WppCore.ActivityChangeState.ChangeType type) {
         WppCore.listenerAcitivity.forEach((listener) -> listener.onChange(activity, type));
     }
@@ -15,42 +17,42 @@ public class WaCallback implements Application.ActivityLifecycleCallbacks {
     @Override
     public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle bundle) {
         WppCore.mCurrentActivity = activity;
+        ActivityStateRegistry.updateState(activity, WppCore.ActivityChangeState.ChangeType.CREATED);
         triggerActivityState(activity, WppCore.ActivityChangeState.ChangeType.CREATED);
-        WppCore.activities.add(activity);
     }
 
     @Override
     public void onActivityStarted(@NonNull Activity activity) {
         WppCore.mCurrentActivity = activity;
+        ActivityStateRegistry.updateState(activity, WppCore.ActivityChangeState.ChangeType.STARTED);
         triggerActivityState(activity, WppCore.ActivityChangeState.ChangeType.STARTED);
-        WppCore.activities.add(activity);
     }
 
     @Override
     public void onActivityResumed(@NonNull Activity activity) {
         WppCore.mCurrentActivity = activity;
-        WppCore.activities.add(activity);
+        ActivityStateRegistry.updateState(activity, WppCore.ActivityChangeState.ChangeType.RESUMED);
         triggerActivityState(activity, WppCore.ActivityChangeState.ChangeType.RESUMED);
     }
 
     @Override
     public void onActivityPaused(@NonNull Activity activity) {
+        ActivityStateRegistry.updateState(activity, WppCore.ActivityChangeState.ChangeType.PAUSED);
         triggerActivityState(activity, WppCore.ActivityChangeState.ChangeType.PAUSED);
     }
 
     @Override
     public void onActivityStopped(@NonNull Activity activity) {
+        ActivityStateRegistry.updateState(activity, WppCore.ActivityChangeState.ChangeType.ENDED);
         triggerActivityState(activity, WppCore.ActivityChangeState.ChangeType.ENDED);
-        WppCore.activities.remove(activity);
     }
 
     @Override
     public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle bundle) {
-
     }
 
     @Override
     public void onActivityDestroyed(@NonNull Activity activity) {
-        WppCore.activities.remove(activity);
+        ActivityStateRegistry.cleanup();
     }
 }
