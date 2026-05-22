f34660d136af467ad5355d049bd50319a50a2cc2
RH71 <67680354+Kawansejalanproduction2019@users.noreply.github.com>
2026-05-22 23:03:21 +0700

    Add files via upload

---
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/listeners/ConversationItemListener.kt
@@ -0,0 +1,136 @@
+package com.wmods.wppenhacer.xposed.features.listeners
+
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.HeaderViewListAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import com.wmods.wppenhacer.xposed.core.Feature
+import com.wmods.wppenhacer.xposed.core.WppCore
+import com.wmods.wppenhacer.xposed.core.components.FMessageWpp
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+
+class ConversationItemListener(
+    loader: ClassLoader,
+    preferences: XSharedPreferences
+) : Feature(loader, preferences) {
+
+    companion object {
+        @JvmField
+        val conversationListeners = HashSet<OnConversationItemListener>()
+
+        var adapter: ListAdapter? = null
+
+        private var hooked: XC_MethodHook.Unhook? = null
+
+        @JvmStatic
+        fun notifyDataSetChanged() {
+            Handler(Looper.getMainLooper()).post {
+                (adapter as? BaseAdapter)?.notifyDataSetChanged()
+            }
+        }
+    }
+
+    @Throws(Throwable::class)
+    override fun doHook() {
+        XposedHelpers.findAndHookMethod(
+            ListView::class.java,
+            "setAdapter",
+            ListAdapter::class.java,
+            object : XC_MethodHook() {
+                @Throws(Throwable::class)
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val currentActivity = WppCore.getCurrentActivity()
+                    if (currentActivity == null || currentActivity.javaClass.simpleName != "Conversation") {
+                        return
+                    }
+
+                    val listView = param.thisObject as ListView
+                    if (listView.id != android.R.id.list) {
+                        return
+                    }
+
+                    var currentAdapter = param.args[0] as? ListAdapter
+                    if (currentAdapter is HeaderViewListAdapter) {
+                        currentAdapter = currentAdapter.wrappedAdapter
+                    }
+
+                    if (currentAdapter == null) {
+                        return
+                    }
+
+                    adapter = currentAdapter
+
+                    for (listener in conversationListeners) {
+                        listener.onAttachAdapter(adapter)
+                    }
+
+                    hooked?.unhook()
+
+                    val method = adapter!!.javaClass.getDeclaredMethod(
+                        "getView",
+                        Int::class.javaPrimitiveType,
+                        View::class.java,
+                        ViewGroup::class.java
+                    )
+
+                    hooked = XposedBridge.hookMethod(method, object : XC_MethodHook() {
+                        @Throws(Throwable::class)
+                        override fun afterHookedMethod(param: MethodHookParam) {
+                            if (param.thisObject !== adapter) return
+
+                            val position = param.args[0] as Int
+                            val convertView = param.args[1] as? View
+                            val viewGroup = param.result as? ViewGroup ?: return
+
+                            val fMessageObj = adapter!!.getItem(position) ?: return
+
+                            val fMessage = FMessageWpp(fMessageObj)
+
+                            for (listener in conversationListeners) {
+                                try {
+                                    listener.onItemBind(fMessage, viewGroup, position, convertView)
+                                } catch (e: Throwable) {
+                                    logDebug(e)
+                                }
+                            }
+                        }
+                    })
+                }
+            }
+        )
+    }
+
+    override fun getPluginName(): String {
+        return "Conversation Item Listener"
+    }
+
+    abstract class OnConversationItemListener {
+        /**
+         * Called when a message item is rendered in the conversation
+         *
+         * @param fMessage The message
+         * @param view     The view associated with the item
+         * @param position The position
+         * @param convertView The view from the adapter
+         * @throws Throwable Errors caught in the hook
+         */
+        @Throws(Throwable::class)
+        abstract fun onItemBind(
+            fMessage: FMessageWpp,
+            view: ViewGroup,
+            position: Int,
+            convertView: View?
+        )
+
+        open fun onAttachAdapter(adapter: ListAdapter?) {
+            // TODO
+        }
+    }
+}
---
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/listeners/MenuStatusListener.kt
@@ -0,0 +1,115 @@
+package com.wmods.wppenhacer.xposed.features.listeners
+
+import android.view.Menu
+import android.view.MenuItem
+import com.wmods.wppenhacer.xposed.core.Feature
+import com.wmods.wppenhacer.xposed.core.components.StatusItemWpp
+import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator
+import com.wmods.wppenhacer.xposed.utils.ReflectionUtils
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import org.luckypray.dexkit.query.enums.StringMatchType
+
+class MenuStatusListener(classLoader: ClassLoader, preferences: XSharedPreferences) :
+    Feature(classLoader, preferences) {
+
+    companion object {
+        @JvmStatic
+        val menuStatuses = LinkedHashSet<OnMenuItemStatusListener>()
+
+        @JvmStatic
+        lateinit var statusData: StatusData
+    }
+
+
+    override fun doHook() {
+
+        val menuStatusMethod = Unobfuscator.loadMenuStatusMethod(classLoader)
+        val menuManagerClass = Unobfuscator.loadMenuManagerClass(classLoader)
+
+        val statusPlaybackBaseFragmentClass =Unobfuscator.findFirstClassUsingName(classLoader, StringMatchType.EndsWith,"StatusPlaybackBaseFragment")
+        val statusPlaybackContactFragmentClass = Unobfuscator.findFirstClassUsingName(classLoader, StringMatchType.EndsWith,"StatusPlaybackContactFragment")
+        val listStatusField = ReflectionUtils.getFieldByExtendType(
+            statusPlaybackContactFragmentClass,
+            List::class.java
+        )
+
+        XposedBridge.hookMethod(menuStatusMethod, object : XC_MethodHook() {
+
+            @Throws(Throwable::class)
+            override fun afterHookedMethod(param: MethodHookParam) {
+                val fieldObjects = param.method.declaringClass.declaredFields
+                    .mapNotNull { field -> ReflectionUtils.getObjectField(field, param.thisObject) }
+
+                val fragmentInstance: Any =
+                    if (param.thisObject != null && statusPlaybackContactFragmentClass.isInstance(
+                            param.thisObject
+                        )
+                    ) {
+                        param.thisObject
+                    } else {
+                        fieldObjects.firstOrNull { statusPlaybackBaseFragmentClass.isInstance(it) }
+                            ?: return
+                    }
+
+                val menu: Menu = if (param.args.isNotEmpty() && param.args[0] is Menu) {
+                    param.args[0] as Menu
+                } else {
+                    val menuManager = fieldObjects.firstOrNull { menuManagerClass.isInstance(it) }
+                    val menuField =
+                        ReflectionUtils.getFieldByExtendType(menuManagerClass, Menu::class.java)
+                    ReflectionUtils.getObjectField(menuField, menuManager) as Menu
+                }
+
+                val listStatus = listStatusField?.get(fragmentInstance) as List<*>
+
+                statusData = StatusData(listStatus, fragmentInstance)
+
+                for (menuStatus in menuStatuses) {
+                    val menuItem = menuStatus.addMenu(menu, statusData) ?: continue
+
+                    menuItem.setOnMenuItemClickListener { item ->
+                        menuStatus.onClick(item, statusData)
+                        true
+                    }
+                }
+            }
+        })
+    }
+
+    override fun getPluginName(): String {
+        return "Menu Status"
+    }
+
+    open class StatusData(private val listStatus: List<*>, private val fragmentInstance: Any) {
+
+        private var cachedItemList: List<StatusItemWpp>? = null
+
+        val currentItem: StatusItemWpp
+            get() = getCurrentItemList()[currentIndex]
+
+        val currentIndex: Int
+            get() = XposedHelpers.getObjectField(fragmentInstance, "A00") as Int
+
+        fun getCurrentItemList(): List<StatusItemWpp> {
+            return cachedItemList ?: listStatus.mapNotNull { obj ->
+                StatusItemWpp.from(obj)
+            }.also { cachedItemList = it }
+        }
+    }
+
+    abstract class OnMenuItemStatusListener {
+
+        abstract fun addMenu(
+            menu: Menu,
+            statusData: StatusData,
+        ): MenuItem?
+
+        abstract fun onClick(
+            item: MenuItem,
+            statusData: StatusData
+        )
+    }
+}
