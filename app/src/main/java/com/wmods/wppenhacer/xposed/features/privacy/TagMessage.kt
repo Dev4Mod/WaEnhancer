6c000ab04c90c3862396d2c7785119cec681c1e5
Dev4Mod <wellingtonmods@gmail.com>
2026-06-06 09:54:31 -0300

    refactor(TagMessage): convert from Java to Kotlin

--- app/src/main/java/com/wmods/wppenhacer/xposed/features/privacy/TagMessage.kt
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/privacy/TagMessage.kt
@@ -1,84 +1,77 @@
-package com.wmods.wppenhacer.xposed.features.privacy;
+package com.wmods.wppenhacer.xposed.features.privacy
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.wmods.wppenhacer.xposed.core.Feature
+import com.wmods.wppenhacer.xposed.core.components.FMessageWpp
+import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator.getMethodDescriptor
+import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator.loadForwardClassMethod
+import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator.loadForwardTagMethod
+import com.wmods.wppenhacer.xposed.features.listeners.ConversationItemListener
+import com.wmods.wppenhacer.xposed.features.listeners.ConversationItemListener.OnConversationItemListener
+import com.wmods.wppenhacer.xposed.utils.DesignUtils
+import com.wmods.wppenhacer.xposed.utils.ReflectionUtils
+import com.wmods.wppenhacer.xposed.utils.Utils
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XSharedPreferences
+import de.robv.android.xposed.XposedBridge
 
-import androidx.annotation.NonNull;
+class TagMessage(loader: ClassLoader, preferences: XSharedPreferences) :
+    Feature(loader, preferences) {
 
-import com.wmods.wppenhacer.xposed.core.Feature;
-import com.wmods.wppenhacer.xposed.core.components.FMessageWpp;
-import com.wmods.wppenhacer.xposed.core.devkit.Unobfuscator;
-import com.wmods.wppenhacer.xposed.features.listeners.ConversationItemListener;
-import com.wmods.wppenhacer.xposed.utils.DesignUtils;
-import com.wmods.wppenhacer.xposed.utils.ReflectionUtils;
-import com.wmods.wppenhacer.xposed.utils.Utils;
 
-import java.lang.reflect.Method;
+    override fun doHook() {
+        val method = loadForwardTagMethod(classLoader)
+        logDebug(getMethodDescriptor(method))
+        val forwardClass = loadForwardClassMethod(classLoader)
+        logDebug("ForwardClass: " + forwardClass.name)
 
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XSharedPreferences;
-import de.robv.android.xposed.XposedBridge;
+        XposedBridge.hookMethod(method, object : XC_MethodHook() {
 
-public class TagMessage extends Feature {
-    public TagMessage(ClassLoader loader, XSharedPreferences preferences) {
-        super(loader, preferences);
-    }
-
-    @Override
-    public void doHook() throws Exception {
-
-        Method method = Unobfuscator.loadForwardTagMethod(classLoader);
-        logDebug(Unobfuscator.getMethodDescriptor(method));
-        Class<?> forwardClass = Unobfuscator.loadForwardClassMethod(classLoader);
-        logDebug("ForwardClass: " + forwardClass.getName());
-
-        XposedBridge.hookMethod(method, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if (!prefs.getBoolean("hidetag", false)) return;
-                var arg = (long) param.args[0];
-                if (arg == 1) {
+            override fun beforeHookedMethod(param: MethodHookParam) {
+                if (!prefs.getBoolean("hidetag", false)) return
+                val arg = param.args[0] as Long
+                if (arg == 1L) {
                     if (ReflectionUtils.isCalledFromClass(forwardClass)) {
-                        param.args[0] = 0;
+                        param.args[0] = 0
                     }
                 }
             }
-        });
+        })
 
         if (prefs.getBoolean("broadcast_tag", false)) {
-            hookBroadcastView();
+            hookBroadcastView()
         }
     }
 
-    private void hookBroadcastView() throws Exception {
-
-        ConversationItemListener.conversationListeners.add(new ConversationItemListener.OnConversationItemListener() {
-            @Override
-            public void onItemBind(FMessageWpp fMessage, ViewGroup view, int position, View convertView) {
-                if (fMessage.getKey().isFromMe) return;
-                var dateTextView = (TextView) view.findViewById(Utils.getID("date", "id"));
-                if (dateTextView == null) return;
-                var dateWrapper = (ViewGroup) dateTextView.getParent();
-                int id = Utils.getID("broadcast_icon", "id");
-                View res = dateWrapper.findViewById(id);
-                if (fMessage.isBroadcast() && res == null) {
-                    var broadcast = new ImageView(dateWrapper.getContext());
-                    broadcast.setId(id);
-                    broadcast.setImageDrawable(DesignUtils.getDrawableByName("broadcast_status_icon"));
-                    dateWrapper.addView(broadcast, 0);
+    private fun hookBroadcastView() {
+        ConversationItemListener.conversationListeners.add(object : OnConversationItemListener() {
+            override fun onItemBind(
+                fMessage: FMessageWpp,
+                view: ViewGroup,
+                position: Int,
+                convertView: View?
+            ) {
+                if (fMessage.key.isFromMe) return
+                val dateTextView = view.findViewById<TextView>(Utils.getID("date", "id")) ?: return
+                val dateWrapper = dateTextView.parent as ViewGroup
+                val id = Utils.getID("broadcast_icon", "id")
+                val res = dateWrapper.findViewById<View?>(id)
+                if (fMessage.isBroadcast && res == null) {
+                    val broadcast = ImageView(dateWrapper.context)
+                    broadcast.id = id
+                    broadcast.setImageDrawable(DesignUtils.getDrawableByName("broadcast_status_icon"))
+                    dateWrapper.addView(broadcast, 0)
                 } else if (res != null) {
-                    dateWrapper.removeView(res);
+                    dateWrapper.removeView(res)
                 }
             }
-        });
-
+        })
     }
 
-    @NonNull
-    @Override
-    public String getPluginName() {
-        return "Tag Message";
+    override fun getPluginName(): String {
+        return "Tag Message"
     }
 }
