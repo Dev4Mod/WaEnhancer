f24dd55e20863c0fdd236ba225b495c060ce5935
Dev4Mod <wellingtonmods@gmail.com>
2026-06-07 17:10:09 -0300

    chore(build): remove x86 and x86_64 ABI filters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,6 @@ android {
         ndk {
             abiFilters.add("armeabi-v7a")
             abiFilters.add("arm64-v8a")
-            abiFilters.add("x86_64")
-            abiFilters.add("x86")
         }
 
     }
